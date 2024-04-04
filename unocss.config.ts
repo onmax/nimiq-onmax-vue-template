cb008e29775ec30a2b09aaf756c0a5bb5a8fede8
Maxi <maximogarciamtnez@gmail.com>
2024-04-04 17:14:36 +0200

    added nimiq-css preset

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,7 +1,13 @@
+import { presetNimiq } from 'nimiq-css'
 import { defineConfig, presetUno } from 'unocss'
+import { presetRemToPx } from '@unocss/preset-rem-to-px'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetNimiq({
+      utilities: true,
+    }),
+    presetRemToPx(),
   ],
 })
