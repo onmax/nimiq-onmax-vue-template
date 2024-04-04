2a3027ab78dfc548eb99725bd0c9b68418e3185d
Maxi <maximogarciamtnez@gmail.com>
2024-04-04 16:02:06 +0200

    added unocss

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    UnoCSS(),
   ],
   resolve: {
     alias: {
