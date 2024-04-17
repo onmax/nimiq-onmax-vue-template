ea1f30227c076d63cb2c7f6d0f0e9244dd685cdc
Maxi <maximogarciamtnez@gmail.com>
2024-04-18 00:16:55 +0200

    added some vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import UnoCSS from 'unocss/vite'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 // import checker from 'vite-plugin-checker'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +20,31 @@ export default defineConfig({
     wasm(),
     topLevelAwait(),
     // checker({ typescript: true, vueTsc: true }),
+
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+        VueRouterAutoImports,
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/stores',
+      ],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
   ],
   resolve: {
     alias: {
